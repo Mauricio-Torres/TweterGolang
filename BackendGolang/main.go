92030cdb3ed038d6b7560cd7e658dbb4c7265ba3
Mauricio-Torres <44648203+Mauricio-Torres@users.noreply.github.com>
2020-05-17 02:13:39 -0500

    main de go creado

--- BackendGolang/main.go
+++ BackendGolang/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Mauricio-Torres/TweterGolang/db"
+	"github.com/Mauricio-Torres/TweterGolang/handlers"
+)
+
 func main() {
 
+	if db.CheckConnection() == 0 {
+		log.Fatal("Sin conexion a la DB")
+	}
+
+	handlers.Manejador()
 }
